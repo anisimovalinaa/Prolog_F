d8e0a211ec1eb548d7be28a10c67a05f1655323b
anisimovalinaa <anisimova.linaa@yandex.ru>
2020-04-15 12:40:42 +0300

    Update IZ4.pl

--- IZ4.pl
+++ IZ4.pl
@@ -3,21 +3,40 @@ in_list([_|T],El):-in_list(T,El).
 
 pr:- Coupe=[_,_,_,_],
 
-    in_list(Coupe,[_,poet,play]),
     in_list(Coupe,[alekseev,_,_]),
     in_list(Coupe,[borisov,_,_]),
-    in_list(Coupe,[konst,_,_]),
-    in_list(Coupe,[dmitr,_,_]),
-    not(in_list(Coupe,[_,astr,astro])),
+    in_list(Coupe,[konstantinov,_,_]),
+    in_list(Coupe,[dmitriev,_,_]),
+
+    in_list(Coupe,[_,_,astronomia]),
+    in_list(Coupe,[_,_,proza]),
+    in_list(Coupe,[_,_,poetry]),
+    in_list(Coupe,[_,poet,play]),
+
+    in_list(Coupe,[_,prozaik,_]),
+    in_list(Coupe,[_,astronom,_]),
+    in_list(Coupe,[_,dramaturg,_]),
+
+    (in_list(Coupe,[dmitriev,poet,_]),
+    (in_list(Coupe,[alekseev,astronom,poetry]),not(in_list(Coupe,[borisov,_,astronomia]));
+    in_list(Coupe,[alekseev,prozaik,poetry]),not(in_list(Coupe,[borisov,_,proza]));
+    in_list(Coupe,[alekseev,dramaturg,poetry]),not(in_list(Coupe,[borisov,_,play])));
+
+    in_list(Coupe,[dmitriev,astronom,_]),
+   (in_list(Coupe,[alekseev,poet,astronomia]),not(in_list(Coupe,[borisov,_,poetry]));
+    in_list(Coupe,[alekseev,prozaik,astronomia]),not(in_list(Coupe,[borisov,_,proza]));
+    in_list(Coupe,[alekseev,dramaturg,astronomia]),not(in_list(Coupe,[borisov,_,play])));
+
+    in_list(Coupe,[dmitriev,dramaturg,_]),
+   (in_list(Coupe,[alekseev,poet,play]),not(in_list(Coupe,[borisov,_,poetry]));
+    in_list(Coupe,[alekseev,prozaik,play]),not(in_list(Coupe,[borisov,_,proza]));
+    in_list(Coupe,[alekseev,astronom,play]),not(in_list(Coupe,[borisov,_,astronomia])))),
+
+    not(in_list(Coupe,[_,prozaik,astronomia])),
     not(in_list(Coupe,[_,prozaik,proza])),
-    not(in_list(Coupe,[_,prozaik,astro])),
-    not(in_list(Coupe,[_,prozaik,play])),
-    in_list(Coupe,[_,prozaik,poetry]),
-    in_list(Coupe,[_,astr,proza]),
-    in_list(Coupe,[_,drama,astro]),
-    not(in_list(Coupe,[dmitr,prozaik,_])),
+    not(in_list(Coupe,[_,astronom,astronomia])),
     not(in_list(Coupe,[alekseev,_,proza])),
-    not(in_list(Coupe,[dmitr,_,poetry])),
+    not(in_list(Coupe,[dmitriev,prozaik,_])),
 
     write(Coupe).
 
