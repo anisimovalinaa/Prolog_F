056e29a8935e36d4e474632c41d03d54788d3ec5
anisimovalinaa <anisimova.linaa@yandex.ru>
2020-04-23 10:48:20 +0300

    Update IZ9.fs

--- IZ9.fs
+++ IZ9.fs
@@ -15,46 +15,27 @@ let pr x =
     | 2 -> true
     | _ -> pr1 x 2
 
-let f x =
-    x = true
-
-let check1 x =
-    let k = false
-    for i in (x-1) .. -1 .. 2 do
-        if pr i then let b : double = Math.Sqrt(Convert.ToDouble(x-i)/2.0)
-                     if (Convert.ToDouble(Convert.ToInt32(b))=b && k=false) then f k
-                                                                      //System.Console.Write(x)
-                                                                      //System.Console.Write("=")
-                                                                      //System.Console.Write(i)
-                                                                      //System.Console.Write("+2*")
-                                                                      //System.Console.Write(b)
-                                                                      //System.Console.WriteLine("^2")
-                                                                                 System.Console.WriteLine(x)
-                                                                                 
-    if k = false then false
-    else true
-
 let check x = 
     let mutable k = false
     for i in (x-1) .. -1 .. 2 do
-        if pr i then let b : double = Math.Sqrt(Convert.ToDouble(x-i)/2.0)
-                     if (Convert.ToDouble(Convert.ToInt32(b))=b && k=false) then k <- true
+        if pr i && k=false then let b : double = Math.Sqrt(Convert.ToDouble(x-i)/2.0)
+                                if (Convert.ToDouble(Convert.ToInt32(b))=b) then k <- true
                                                                       //System.Console.Write(x)
                                                                       //System.Console.Write("=")
                                                                       //System.Console.Write(i)
                                                                       //System.Console.Write("+2*")
                                                                       //System.Console.Write(b)
-                                                                      //System.Console.WriteLine("^2")
-        
-                                                                                     
+                                                                      //System.Console.WriteLine("^2")                                                                                     
     if k = false then false
     else true
 
 [<EntryPoint>]
 let main argv =
-    //let x = System.Convert.ToInt32(System.Console.ReadLine())   
+    //let x = System.Convert.ToInt32(System.Console.ReadLine())  
+    let mutable f = false
     for i in 2..6000 do
-        if (i%2<>0) && not(pr i) then
+        if (i%2<>0) && not(pr i) && f=false then
             if not(check i) then System.Console.WriteLine(i)
+                                 f <- true
 
     0 // return an integer exit code
