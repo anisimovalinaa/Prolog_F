711b7cd0215a4189dd0c49397f0e4197efda94c3
anisimovalinaa <anisimova.linaa@yandex.ru>
2020-05-12 17:19:25 +0300

    Update

--- IZ13/IZ13/Program.fs
+++ IZ13/IZ13/Program.fs
@@ -34,6 +34,35 @@ let rec createList a b list =
     | _ -> if (a.[0] = ' ') then createList (a.Remove(0,1)) "" (Convert.ToInt32(b)::list)
            else createList (a.Remove(0,1)) (b+(a.[0].ToString())) list
 
+let rec function1 l1 l2 subList (subList1: 'a list) rezList max = 
+    match l1, l2 with  
+    | _, [] -> if (max < subList1.Length) then subList1
+               else rezList
+    | [], _ -> if (max < subList1.Length) then function1 subList l2.Tail subList [] subList1 subList1.Length
+               else function1 subList l2.Tail subList [] rezList max
+    | _, _ -> if (l1.Head = l2.Head) then function1 l1.Tail l2.Tail subList (l1.Head::subList1) rezList max
+              else if (max < subList1.Length) then function1 subList l2.Tail subList [] subList1 subList1.Length
+                   else function1 subList l2.Tail subList [] rezList max
+
+let rec function2 listF (listS: 'a list) resultList max = 
+    match listF with
+    | [] -> resultList
+    | _ -> let listR = function1 listF listS listF [] [] max
+           if (listR.Length>max) then function2 listF.Tail listS listR listR.Length
+           else function2 listF.Tail listS resultList max
+
+let f1 list1 list2 = 
+    let r = List.map (fun x -> function1 list1 list2 list1 [] [] 0) list1
+    //let r = List.fold (fun acc x -> x::acc) [] list1
+    r
+
+let new_list list1 list2 =
+    match list1, list2 with
+    | [], [] -> []
+    | [], _ -> []
+    | _, [] -> []
+    | _, _ -> function2 list1 list2 [] 0
+
 form.Controls.Add(label1)
 form.Controls.Add(label2)
 form.Controls.Add(label3)
@@ -46,7 +75,8 @@ button.Click.Add(fun evArgs -> let a = posled1.Text
                                let b = posled2.Text
                                let list1 = createList a "" []
                                let list2 = createList b "" []
-                               //List.iter (fun x -> answer.AppendText(x.ToString() + " ")) (createList b "" [])
+                               let rezList = f1 list1 list2
+                               List.iter (fun x -> answer.AppendText(x.ToString() + " ")) rezList
                                |> ignore)
 
 do Application.Run(form)
