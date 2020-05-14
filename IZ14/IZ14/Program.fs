08bfc29071fe2b6d38ed87933e45485d5cbf5e1f
anisimovalinaa <anisimova.linaa@yandex.ru>
2020-05-14 17:40:39 +0300

    Update

--- IZ14/IZ14/Program.fs
+++ IZ14/IZ14/Program.fs
@@ -3,10 +3,14 @@
 open System
 open System.Drawing
 open System.Windows.Forms
+open System.IO
 
 let form = new Form(Text = "Индивидуальная работа №14.", Height = 500, Width = 500)
 
-let textString = new RichTextBox(Height=100, Width = 100)
+let buttonOpen = new Button(Text = "Открыть файл", Width = 100)
+buttonOpen.Location <- new Point(165, 20)
+
+let textString = new RichTextBox(Height = 100, Width = 400)
 textString.Location <- new Point(50,50)
 
 let label1 = new Label(Text = "Введите слово: ")
@@ -24,6 +28,7 @@ label2.Location <- new Point(120, 280)
 let answer = new RichTextBox(Height = 20, Width = 60)
 answer.Location <- new Point(180, 280)
 
+form.Controls.Add(buttonOpen)
 form.Controls.Add(textString)
 form.Controls.Add(label1)
 form.Controls.Add(textWord)
@@ -39,9 +44,15 @@ let func (word1: string) (word2: string) =
 let rec countPemutations (text: string) (word: string) iter (word1: string) count =
     match iter = text.Length with
     | true -> count
-    | false -> if(text.[iter] <> ' ') then countPemutations text word (iter+1) (word1+text.[iter].ToString()) count
+    | false -> if(text.[iter] <> '\n' && text.[iter] <> ' ') then countPemutations text word (iter+1) (word1+text.[iter].ToString()) count
                else if (func word word1) then countPemutations text word (iter+1) "" (count+1)
                        else countPemutations text word (iter+1) "" count
+
+[<STAThread>]
+buttonOpen.Click.Add (fun evArgs -> let openFile = new OpenFileDialog():OpenFileDialog
+                                    if (openFile.ShowDialog() = DialogResult.OK) then let file = new StreamReader(openFile.FileName)
+                                                                                      textString.AppendText(file.ReadToEnd())
+                                    else textString.AppendText("")|> ignore)
 
 button.Click.Add(fun evArgs -> let a = textString.Text
                                let b = textWord.Text
